5d7597a1d0d007b9eb188c3106b339d46aaf7a2e
oliveirab <brunno.oliveira@me.com>
2023-12-21 10:41:14 +0100

    using code from https://gist.github.com/scbrown86/2779137a9378df7b60afd23e0c45c188

--- R/colmat.R
+++ R/colmat.R
@@ -6,34 +6,21 @@
 #' # https://rfunctions.blogspot.com/2015/03/bivariate-maps-bivariatemap-function.html
 #' @description Creates a color matrix to be used in bivariate.map() function.
 #' @return Two outputs: a color matrix object to be used in bivariate.map() function, and a plot of the color matrix.
-#' @param nbreaks numeric variable for number of quantiles in color matrix
+#' @param nbreaks numeric variable for number of breaks in color matrix
+#' @param breakstyle character variable for style used compute class intervals in color matrix. This is uses classInt::classIntervals. Options are one of "fixed", "sd", "equal", "pretty", "quantile", "kmeans", "hclust", "bclust", "fisher", "jenks", "dpih", "headtails", "maximum", or "box".
 #' @param upperleft upperleft color of color matrix
 #' @param upperright upperright color of color matrix
 #' @param bottomleft bottomleft color of color matrix
 #' @param bottomright bottomright color of color matrix
 #' @param xlab character variable
 #' @param ylab character variable
-# colmat<-function(nbreaks=10, upperleft="blue", upperright="red", bottomleft="grey", bottomright="yellow", xlab="x label", ylab="y label"){
-#   my.data<-seq(0,1,.01)
-#   my.class<-classInt::classIntervals(my.data,n=nbreaks,style="quantile")
-#   my.pal.1<-classInt::findColours(my.class,c(upperleft,bottomleft))
-#   my.pal.2<-classInt::findColours(my.class,c(upperright, bottomright))
-#   col.matrix<-matrix(nrow = 101, ncol = 101, NA)
-#   for(i in 1:101){
-#     my.col<-c(paste(my.pal.1[i]),paste(my.pal.2[i]))
-#     col.matrix[102-i,]<-classInt::findColours(my.class,my.col)}
-#   seqs<-seq(0,100,(100/nbreaks))
-#   seqs[1]<-1
-#   col.matrix<-col.matrix[c(seqs), c(seqs)]
-#   return(col.matrix)
-# }
-
-colmat <- function(nbreaks = 10, breakstyle = "quantile",
+#' @param plotLeg default TRUE, if FALSE, does not plot the color matrix when function ends
+#' @param saveLeg default TRUE, if FALSE, does not save a ggplot file of the color matrix when function ends
+colmat <- function(nbreaks = 3, breakstyle = "quantile",
                    upperleft = "#0096EB", upperright = "#820050",
                    bottomleft = "#BEBEBE", bottomright = "#FFE60F",
                    xlab = "x label", ylab = "y label", plotLeg = TRUE,
-                   saveLeg = FALSE) {
-  # TODO - replace any tidyr, dplyr etc. functions with data.table #
+                   saveLeg = TRUE) {
   library(tidyverse)
   require(ggplot2)
   require(classInt)
@@ -84,33 +71,32 @@ colmat <- function(nbreaks = 10, breakstyle = "quantile",
            "X" = rep(seq(from = 1, to = nbreaks, by = 1), times = nbreaks)) %>%
     mutate("UID" = row_number())
   # Use plotLeg if you want a preview of the legend
-  if (any(plotLeg,saveLeg)) {
-    p <- ggplot(col.matrix.plot, aes((X*2)/10, (Y*2)/10, fill = HEXCode)) +
-      geom_raster() +
+  if (plotLeg) {
+    p <- ggplot(col.matrix.plot, aes(X, Y, fill = HEXCode)) +
+      geom_tile() +
       scale_fill_identity() +
       coord_equal(expand = FALSE) +
-      theme_minimal() +
-      scale_x_continuous(breaks = seq(0, 1, by=0.2)) +
-      scale_y_continuous(breaks = seq(0, 1, by=0.2))+
+      theme_void() +
       theme(aspect.ratio = 1,
-            axis.title = element_text(size = 10, colour = "black",hjust = 0.5,
+            axis.title = element_text(size = 12, colour = "black",hjust = 0.5,
                                       vjust = 1),
-            axis.text = element_text(size = 8),
             axis.title.y = element_text(angle = 90, hjust = 0.5)) +
       xlab(bquote(.(xlab) ~  symbol("\256"))) +
       ylab(bquote(.(ylab) ~  symbol("\256")))
-    p
+    print(p)
   }
   seqs <- seq(0, 100, (100 / nbreaks))
   seqs[1] <- 1
   col.matrix <- col.matrix[c(seqs), c(seqs)]
   attr(col.matrix, "breakstyle") <- breakstyle
   attr(col.matrix, "nbreaks") <- nbreaks
-
+  # Use saveLeg if you want to save a copy of the legend
   if (saveLeg) {
-    return(list(col.matrix,p))
+    return(list(col.matrix = col.matrix,
+                plot = p))
   } else {
-    return(list(col.matrix,NULL))
+    return(list(col.matrix = col.matrix,
+                plot = NULL))
   }
 }
 
